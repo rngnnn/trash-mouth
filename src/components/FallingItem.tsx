c36d53e247c082e5c3c43f5c324059ddb154b999
rngnnn <rngnnnclk@gmail.com>
2025-05-07 23:40:29 +0200

    Fix item type declaration in FallingItem component: remove duplicate entry for 'needle'

--- src/components/FallingItem.tsx
+++ src/components/FallingItem.tsx
@@ -4,7 +4,7 @@ type ItemType =
   | 'apple' | 'kitty' | 'kitty2' | 'horse' | 'people' | 'chip'
   | 'flower1' | 'flower2' | 'flower3' | 'controller1' | 'controller2'
   | 'game' | 'camera' | 'photography'
-  | 'needle' | 'needle2' | 'ghost1' | 'ghost2' | 'forbidden1'
+  |  | 'needle2' | 'ghost1' | 'ghost2' | 'forbidden1'
   | 'forbidden2' | 'forbidden3' | 'robot' | 'robot2' | 'shark'
   | 'stain' | 'moon' | 'ice' | 'brush1' | 'brush2' | 'brush3'
   | 'key1' | 'key2' | 'minecraft' | 'block' | 'phone' | 'camera2';
